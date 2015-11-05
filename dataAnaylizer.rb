fd167bb4dd652a2b789dd58f1a75a7bef67c178c
Charles Lambert <c.lambert1213@gmail.com>
2015-11-05 09:17:36 -0800

    v16

---
+++ dataAnaylizer.rb
@@ -0,0 +1,68 @@
+#!/usr/bin/env ruby
+require 'csv'
+
+random = Random.new
+hash = {:insert => [], 
+		:select => [], 
+		:bubble => [], 
+		:merge 	=> []}
+system("make all")
+xarray = []
+
+for i in (100..10000).step(100)
+	xarray.push(i)
+end
+
+
+for i in (100..10000).step(100) 
+#generate random data to be sorted
+#creates files to hold data
+	f = File.new("num#{i}","w")
+	for j in 0...i
+		f.print "#{random.rand(100)},"
+	end
+	f.close
+end
+
+for i in (100..10000).step(100)
+	10.times do
+		system("./sort insert num#{i}")
+		system("./sort select num#{i}")
+		system("./sort bubble num#{i}")
+		system("./sort merge num#{i}")
+	end
+
+	hash.keys.each {|key| 
+		array = []
+		sum = 0
+
+		f = File.open("#{key.to_s}clock.txt","r")
+
+		f.each_line {|x| x.chomp.split(",").map { |y|
+			array.push(y.to_f)}}
+		f.close
+		
+		array.each {|x| sum += x}
+
+		hash[key].push(sum/array.length)
+
+		File.delete("#{key.to_s}clock.txt")
+	}
+
+end
+
+hash.keys.each { |key|
+	CSV.open("#{key.to_s}.csv","w"){ |csv|
+		for i in 0...xarray.length
+			csv << [xarray[i], hash[key][i]]
+		end
+		}
+	}
+
+p hash[:insert].length == xarray.length
+
+for i in (100..10000).step(100)
+#clean up all files
+	File.delete("num#{i}")
+end
+system("make clean")
---
+++ dataGrapher.rb
@@ -0,0 +1,16 @@
+require 'mkmf'
+
+def gnuplot(commands)
+  IO.popen(find_executable('gnuplot'), "w") { |io| io.puts commands }
+end
+
+commands = %Q(
+  set terminal svg
+  set output "curves.svg"
+  set datafile separator ","
+  set autoscale
+  set xlabel "n"
+  set ylabel "T(n)"
+  plot 'select.csv' u 1:2 w l title 'select','insert.csv' u 1:2 w l title 'insert','bubble.csv' u 1:2 w l title 'bubble','merge.csv' u 1:2 w l title 'merge',
+)
+gnuplot(commands)
