93ae119ea84b74548f499ec5af3834971e03f93e
phil0s0pher <c.lambert1213@gmail.com>
2015-11-24 18:43:19 -0800

    heapsort vF

--- dataGrapher.rb
+++ dataGrapher.rb
@@ -1,16 +1,16 @@
 require 'mkmf'
 
 def gnuplot(commands)
-  IO.popen(find_executable('gnuplot'), "w") { |io| io.puts commands }
+  IO.popen(find_executable("gnuplot.exe"), "w") { |io| io.puts commands }
 end
 
 commands = %Q(
-  set terminal svg
-  set output "curves.svg"
+  set terminal wxt
+  set output
   set datafile separator ","
   set autoscale
   set xlabel "n"
   set ylabel "T(n)"
-  plot 'select.csv' u 1:2 w l title 'select','insert.csv' u 1:2 w l title 'insert','bubble.csv' u 1:2 w l title 'bubble','merge.csv' u 1:2 w l title 'merge', 'heap'
+  plot 'select.csv' u 1:2 w l title 'select','insert.csv' u 1:2 w l title 'insert','bubble.csv' u 1:2 w l title 'bubble','merge.csv' u 1:2 w l title 'merge', 'heap.csv' u 1:2 w l title 'heap'
 )
 gnuplot(commands)
